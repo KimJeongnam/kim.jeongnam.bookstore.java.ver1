05f76610886e7248cfe1ae25a33324d0664eecf7
KimJeongnam <kim910712@gmail.com>
2018-10-12 17:08:27 +0900

    주석 수정 v1.1

--- src/domain/Session.java
+++ src/domain/Session.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Session {
-	private Map<String, String> map = new HashMap<String, String>();   // key= id , value = password
+	private Map<String, String> map = new HashMap<String, String>();   // key= "id" , value = userid
 	
 	public Map<String, String> getMap(){ return map; }
 	
